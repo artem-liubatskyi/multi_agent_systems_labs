6f5b7cf559ec00b7d8548a94f2c98cb3ec83677f
Artem Liubatskyi <deadly241@gmail.com>
2022-11-06 21:38:42 +0200

    Added arguments string builder

--- lab_1/src/Main.java
+++ lab_1/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        String[] jadeArgs = {"-gui", "-agents", "buyer:BookBuyerAgent; seller:BookSellerAgent"};
+        String[] agentNames = {"BookBuyerAgent", "BookSellerAgent"};
+        String[] jadeArgs = {"-gui", "-agents", buildAgentsArgument(agentNames)};
         jade.Boot.main(jadeArgs);
+    }
+
+    private static String buildAgentsArgument(String[] agentNames) {
+        String[] result = new String[agentNames.length];
+
+        for (int i = 0; i < agentNames.length; i++) {
+            var agentName = agentNames[i];
+            result[i] = agentName + ":" + agentName;
+        }
+
+        return String.join(";", result);
     }
 }
